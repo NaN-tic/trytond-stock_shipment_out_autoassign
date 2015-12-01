da17087081dfe6cb39efffc6bdc07ade9ed5a2a2
resteve <resteve@zikzakmedia.com>
2015-12-01 11:26:31 +0100

    Tests

--- tests/test_stock_shipment_out_autoassign.py
+++ tests/test_stock_shipment_out_autoassign.py
@@ -1,24 +1,14 @@
-#!/usr/bin/env python
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the stock_shipment_out_autoassign module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends, test_view
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockShipmentOutAutoassignTestCase(unittest.TestCase):
+class StockShipmentOutAutoassignTestCase(ModuleTestCase):
     'Test Stock Shipment Out Autoassign module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_shipment_out_autoassign')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_shipment_out_autoassign')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_shipment_out_autoassign'
 
 
 def suite():
@@ -26,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockShipmentOutAutoassignTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
