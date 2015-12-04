cf4bd4ea04406f0200b076ace93c54b085770b83
jmartin <jmartin@zikzakmedia.com>
2015-12-04 12:24:47 +0100

    Add autoassign_out_moves method to move

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from .move import *
 from .shipment import *
 
 
 def register():
     Pool.register(
+        Move,
         ShipmentOut,
         ShipmentOutAssignWizardStart,
         ShipmentOutAssignWizardShipments,
