ba0a40c1a4522aeba3663660e82acde2b378f76e
Raimon Esteve <raimon@nan-tic.com>
2017-02-07 09:32:41 +0100

    Stock configuration to try assign shipments in wait state

--- __init__.py
+++ __init__.py
@@ -1,16 +1,18 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .move import *
-from .shipment import *
+from . import configuration
+from . import move
+from . import shipment
 
 
 def register():
     Pool.register(
-        Move,
-        ShipmentOut,
-        ShipmentOutAssignWizardStart,
+        configuration.Configuration,
+        move.Move,
+        shipment.ShipmentOut,
+        shipment.ShipmentOutAssignWizardStart,
         module='stock_shipment_out_autoassign', type_='model')
     Pool.register(
-        ShipmentOutAssignWizard,
+        shipment.ShipmentOutAssignWizard,
         module='stock_shipment_out_autoassign', type_='wizard')
